086b5c7c9bf3422f2a4780e93b9e551122e683a3
Alessandro <perrialessandro93@gmail.com>
2023-12-01 16:40:22 +0100

    **_**

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -116,7 +116,6 @@ console.log("COLORI CORRISPONDENTI A BUZZ:", buzzC);
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
-
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
